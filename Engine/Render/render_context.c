33a12992f0eeff9371cfa5b58224967b265d6f92
Jussi Enroos <jussi.enroos@gmail.com>
2021-05-16 15:02:39 +0300

    Roll back rect pooling, as it turned out not effective

--- Engine/Render/render_context.c
+++ Engine/Render/render_context.c
@@ -37,10 +37,10 @@ char *render_context_describe(void *value)
 
 int context_rect_compare_top_edge(const void *a, const void *b)
 {
-    RenderRect *sq_a = *(RenderRect**)a;
-    RenderRect *sq_b = *(RenderRect**)b;
+    RenderRect *rect_a = *(RenderRect**)a;
+    RenderRect *rect_b = *(RenderRect**)b;
     
-    int diff = sq_a->top - sq_b->top;
+    int diff = rect_a->top - rect_b->top;
     
     if (diff < nb_zero) {
         return list_sorted_ascending;
@@ -131,44 +131,46 @@ void context_clean_union_of_rendered_rects(RenderContext *ctx, ArrayList *render
     
     size_t i = 0;
     while (i < count || list_count(ends)) {
-        RenderRect *sq = list_count(rendered_rects) ? list_get(rendered_rects, i) : NULL;
+        RenderRect *next_begin = list_get(rendered_rects, i);
         
         size_t end_count = list_count(ends);
         RenderRect *next_end = (RenderRect *)list_get(ends, end_count - 1);
-        if (next_end && (!sq || next_end->bottom < sq->top)) {
-            // Square ended
-            RenderRect *dropped = NULL;
+        
+        // Line sweep to handle either next beginning of rect or next ending of rect
+        if (next_end && (!next_begin || next_end->bottom < next_begin->top)) {
+            RenderRect *dropped_active = NULL;
             
+            // Find the active rect which ends along with this ending rect, if any
             const size_t actives_count = list_count(actives);
             for (int32_t k = (int32_t)actives_count - 1; k >= 0; --k) {
                 RenderRect *active = list_get(actives, k);
                 
                 if (active->left > next_end->right || active->right < next_end->left) {
-                    // Active does not overlap with ended square
+                    // Active does not overlap with ending rect
                     continue;
                 }
                 
                 if (next_end->left > active->left && next_end->right < active->right && next_end->bottom < active->bottom) {
-                    // Ended square completely contained in this active
+                    // Ending rect is completely contained in this active
                     break;
                 }
 
-                list_drop_index(actives, k);
-                dropped = active;
+                dropped_active = list_drop_index(actives, k);
                 break;
             }
             
             list_drop_index(ends, end_count - 1);
 
-            if (!dropped) {
+            if (!dropped_active) {
                 continue;
             }
             
+            // Merge remaining ending rects into actives
             end_count = list_count(ends);
             for (int32_t k = 0; k < end_count; ++k) {
                 RenderRect *end = list_get(ends, k);
                 
-                if (end->left > dropped->right || end->right < dropped->left) {
+                if (end->left > dropped_active->right || end->right < dropped_active->left) {
                     // This rect completely outside dropped active rect
                     continue;
                 }
@@ -178,93 +180,99 @@ void context_clean_union_of_rendered_rects(RenderContext *ctx, ArrayList *render
                 const size_t temp_count = list_count(merged);
                 RenderRect *first_contact = NULL;
                 for (int t = (int)temp_count - 1; t >= 0; --t) {
-                    RenderRect *temp = list_get(merged, t);
-                    if (end->left > temp->right || end->right < temp->left) {
+                    RenderRect *mergable = list_get(merged, t);
+                    if (end->left > mergable->right || end->right < mergable->left) {
                         // This rect completely outside temp
                         continue;
                     }
                     has_overlapping_temp = true;
                     
                     if (first_contact) {
-                        // This end has already met another temp, join to it
-                        if (temp->left < first_contact->left) {
-                            first_contact->left = temp->left;
+                        // This end has already met another mergable, merge to it
+                        if (mergable->left < first_contact->left) {
+                            first_contact->left = mergable->left;
                         }
-                        if (temp->right > first_contact->right) {
-                            first_contact->right = temp->right;
+                        if (mergable->right > first_contact->right) {
+                            first_contact->right = mergable->right;
                         }
-                        if (temp->bottom < first_contact->bottom) {
-                            first_contact->bottom = temp->bottom;
+                        if (mergable->bottom < first_contact->bottom) {
+                            first_contact->bottom = mergable->bottom;
                         }
                         list_drop_index(merged, t);
-                        context_release_render_rect(ctx, temp);
+                        destroy(mergable);
                         continue;
                     }
                     
-                    // Overlaps with this temp, adjust temp
-                    if (end->left < temp->left) {
-                        temp->left = end->left;
+                    // Overlaps with this mergable, adjust mergable
+                    if (end->left < mergable->left) {
+                        mergable->left = end->left;
                     }
-                    if (end->right > temp->right) {
-                        temp->right = end->right;
+                    if (end->right > mergable->right) {
+                        mergable->right = end->right;
                     }
-                    if (end->bottom < temp->bottom) {
-                        temp->bottom = end->bottom;
+                    if (end->bottom < mergable->bottom) {
+                        mergable->bottom = end->bottom;
                     }
-                    first_contact = temp;
+                    first_contact = mergable;
                 }
                 
                 if (!has_overlapping_temp && end->bottom >= next_end->bottom + 1) {
-                    // Does not overlap with existings temps, create new
-                    list_add(merged, context_get_render_rect(ctx, end->left, end->right, next_end->bottom + 1, end->bottom));
+                    // Does not overlap with existings mergables, create new
+                    list_add(merged, rrect_create(end->left, end->right, next_end->bottom + 1, end->bottom));
                 }
             }
         
             bool keep_dropped = false;
-            // Add temps to actives
+            // Add merged rects to actives
             for (int k = (int)list_count(merged) - 1; k >= 0; --k) {
-                RenderRect *temp = list_drop_index(merged, k);
-                if (dropped->left == temp->left && dropped->right == temp->right && temp->bottom > dropped->bottom) {
+                RenderRect *active_candidate = list_drop_index(merged, k);
+                
+                // This candidate would continue as exactly the same width rect as dropped, so continue dropped instead
+                if (dropped_active->left == active_candidate->left && dropped_active->right == active_candidate->right && active_candidate->bottom > dropped_active->bottom) {
                     keep_dropped = true;
-                    dropped->bottom = temp->bottom;
-                    list_add(actives, dropped);
-                    context_release_render_rect(ctx, temp);
+                    dropped_active->bottom = active_candidate->bottom;
+                    list_add(actives, dropped_active);
+                    destroy(active_candidate);
                 } else {
-                    list_add(actives, temp);
+                    list_add(actives, active_candidate);
                 }
             }
             
             if (!keep_dropped) {
-                // Ended square overlaps with active but not completely contained
-                if (dropped->top <= next_end->bottom) {
-                    list_add(result, context_get_render_rect(ctx, dropped->left, dropped->right, dropped->top, next_end->bottom));
+                // Ended rect overlaps with active but not completely contained
+                if (dropped_active->top <= next_end->bottom) {
+                    list_add(result, rrect_create(dropped_active->left, dropped_active->right, dropped_active->top, next_end->bottom));
                 }
-                context_release_render_rect(ctx, dropped);
+                destroy(dropped_active);
             }
         } else {
+            
+            // Add beginning rect to the list of ending rects
+            // Everything that has a beginning, has an end
             int k;
             for (k = (int)end_count - 1; k >= 0; --k) {
                 RenderRect *end = list_get(ends, k);
-                if (end->bottom > sq->bottom) {
+                if (end->bottom > next_begin->bottom) {
                     break;
                 }
             }
-            list_insert(ends, sq, k + 1);
+            list_insert(ends, next_begin, k + 1);
             
+            // Compare to existing actives, and finish any overlapping ones if not contained
             const size_t actives_count = list_count(actives);
-            int left = sq->left;
-            int right = sq->right;
-            int bottom = sq->bottom;
-            bool sq_is_active = true;
+            int left = next_begin->left;
+            int right = next_begin->right;
+            int bottom = next_begin->bottom;
+            bool rect_is_active = true;
             for (int32_t k = (int32_t)actives_count - 1; k >= 0; --k) {
                 RenderRect *active = list_get(actives, k);
                 
-                if (active->left > sq->right || active->right < sq->left) {
+                if (active->left > next_begin->right || active->right < next_begin->left) {
                     continue;
                 }
                 
-                if (sq->left >= active->left && sq->right <= active->right) {
-                    sq_is_active = false;
+                if (next_begin->left >= active->left && next_begin->right <= active->right) {
+                    rect_is_active = false;
                     break;
                 }
                 
@@ -278,23 +286,19 @@ void context_clean_union_of_rendered_rects(RenderContext *ctx, ArrayList *render
                     bottom = active->bottom;
                 }
                           
-                if (active->top <= sq->top - 1) {
-                    list_add(result, context_get_render_rect(ctx, active->left, active->right, active->top, sq->top - 1));
+                if (active->top <= next_begin->top - 1) {
+                    list_add(result, rrect_create(active->left, active->right, active->top, next_begin->top - 1));
                 }
-                list_drop_index(actives, k);
-                context_release_render_rect(ctx, active);
+
+                destroy(list_drop_index(actives, k));
             }
             
-            if (sq_is_active) {
-                list_add(actives, context_get_render_rect(ctx, left, right, sq->top, bottom));
+            if (rect_is_active) {
+                list_add(actives, rrect_create(left, right, next_begin->top, bottom));
             }
             
             ++i;
         }
-    }
-    
-    if (list_count(actives) || list_count(merged) || list_count(ends)) {
-        LOG_ERROR("clean_union_of_rendered_rects LIST NOT EMPTY WHEN FINISHED");
     }
     
     if (!ctx) {
