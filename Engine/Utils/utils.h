c004aec346ae748169f907958f43a05c78d50bd7
Jussi Enroos <jussi.enroos@gmail.com>
2022-07-19 20:03:03 +0300

    Add for each convenience macros and str compare macro

--- Engine/Utils/utils.h
+++ Engine/Utils/utils.h
@@ -16,4 +16,13 @@
     __typeof__ (b) __b = (b); \
   (__a >= 0 ? __a / __b : ((__a + 1) / __b) - 1); })
 
+#define for_each_begin(type, item, list) { type item = NULL; \
+const size_t for_each_count = list_count(list); \
+for (size_t for_each_index = 0; for_each_index < for_each_count; ++for_each_index) { \
+item = (type)list_get(list, for_each_index);
+
+#define for_each_end }}
+
+#define str_equals(a, b) (strcmp(a, b) == 0)
+
 #endif /* utils_h */
